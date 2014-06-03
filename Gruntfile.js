67712a9580e7a67815a63ea458cb56b82179ecf1
joseph <josephniet@gmail.com>
2014-06-03 16:30:19 +0100

    added grunt bump

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,6 +39,21 @@ module.exports = function (grunt) {
             }]
         }
     }, 
+    bump: {
+        options: {
+          files: ['package.json'],
+          updateConfigs: [],
+          commit: true,
+          commitMessage: 'Release v%VERSION%',
+          commitFiles: ['package.json'],
+          createTag: true,
+          tagName: 'v%VERSION%',
+          tagMessage: 'Version %VERSION%',
+          push: true,
+          pushTo: 'upstream',
+          gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d'
+        }
+    },  
 
     // Watches files for changes and runs tasks based on the changed files
     watch: {
